5aa29679c9ac04dfdc0495a177c4628d933b29ab
Flavius Matis <flavius@fdbk.me>
2012-11-04 22:16:35 +0200

    Updating master.

--- script.js
+++ script.js
@@ -19,10 +19,6 @@ var plugins = [
 				url: "http://www.zurb.com/playground/jquery_image_slider_plugin"
 			},
 			{
-				name: "Beautiful Background Image Navigation with jQuery",
-				url: "http://tympanus.net/codrops/2010/05/05/beautiful-background-image-navigation-with-jquery/"
-			},
-			{
 				name: "jQuery Timelinr",
 				url: "http://www.csslab.cl/2011/08/18/jquery-timelinr/"
 			},
@@ -31,22 +27,30 @@ var plugins = [
 				url: "http://imakewebthings.com/deck.js/"
 			},
 			{
-				name: "jQuery HTML5 Fullscreen Slideshow / Gallery",
-				url: "http://eikes.github.com/jquery.fullscreen.js/"
-			},
-			{
 				name: "Camera | a free jQuery slideshow by Pixedelic",
 				url: "http://www.pixedelic.com/plugins/camera/"
 			},
 			{
 				name: "Flux Slider &#0187; Hardware accelerated image transitions using CSS3",
 				url: "http://www.joelambert.co.uk/flux/"
+			},
+			{
+				name: "Responsive Slides",
+				url: "http://responsive-slides.viljamis.com/"
+			},
+			{
+				name: "Fresco - A beautiful responsive lightbox.",
+				url: "http://www.frescojs.com/"
 			}
 		]
 	},
 	{
 		category: "Maps",
 		items: [
+			{
+				name: "gmaps.js - Google Maps API with less pain and more fun",
+				url: "http://hpneo.github.com/gmaps/"
+			},
 			{
 				name: "bMap - jQuery Plugin",
 				url: "http://www.blocsoft.com/bmap/"
@@ -58,6 +62,10 @@ var plugins = [
 			{
 				name: "jQuery Automatic Geocoder",
 				url: "http://tristandunn.com/projects/jquery-auto-geocoder/"
+			},
+			{
+				name: "Wind Map",
+				url: "http://hint.fm/wind/"
 			}
 		]
 	},
@@ -75,7 +83,16 @@ var plugins = [
 			{
 				name: "Handlebars.js - Minimal Templating on Steroids",
 				url: "http://handlebarsjs.com/"
+			},
+			{
+				name: "Walrus - A bolder kind of mustache.",
+				url: "http://documentup.com/jeremyruppel/walrus/"
+			},
+			{
+				name: "Template Engine Chooser!",
+				url: "http://garann.github.com/template-chooser/"
 			}
+
 		]
 	},
 	{
@@ -108,6 +125,10 @@ var plugins = [
 			{
 				name: "jQuery Smooth Scrolling Plugin",
 				url: "http://pagescroller.com/"
+			},
+			{
+				name: "Flick Scrolling - An idea of a new gesture that extends scrolling content on a touch-screen.",
+				url: "http://simurai.com/post/31460815120/flick-scroll"
 			}
 		]
 	},
@@ -142,10 +163,31 @@ var plugins = [
 			{
 				name: "jQuery URL parser",
 				url: "https://github.com/tombonner/jurlp"
+			}
+		]
+	},
+	{
+		category: "HTML Editors",
+		items: [
+			{
+				name: "Aloha Editor",
+				url: "http://www.aloha-editor.org/index.php"
 			},
 			{
-				name: "jQuery Address - Deep linking for the masses",
-				url: "http://www.asual.com/jquery/address/"
+				name: "Create — Web editing interface",
+				url: "http://createjs.org/"
+			},
+			{
+				name: "Mercury Editor",
+				url: "http://jejacks0n.github.com/mercury/"
+			},
+			{
+				name: "WYSIHTML5 - A better approach to rich text editing",
+				url: "http://xing.github.com/wysihtml5/"
+			},
+			{
+				name: "Redactor - Fantastic WYSIWYG editor on jQuery",
+				url: "http://redactorjs.com/"
 			}
 		]
 	},
@@ -163,6 +205,14 @@ var plugins = [
 			{
 				name: "reMarked.js",
 				url: "http://leeoniya.github.com/reMarked.js/"
+			},
+			{
+				name: "JavaScript Syntax Highlighting Engine",
+				url: "http://mihai.bazon.net/projects/javascript-syntax-highlighting-engine"
+			},
+			{
+				name: "Rainbow - Javascript Code Syntax Highlighting",
+				url: "http://craig.is/making/rainbows"
 			}
 		]
 	},
@@ -210,24 +260,233 @@ var plugins = [
 			{
 				name: "Isotope: An exquisite jQuery plugin for magical layouts",
 				url: "http://isotope.metafizzy.co/"
+			},
+			{
+				name: "Pep - A lightweight plugin for kinetic-drag on mobile/desktop",
+				url: "http://pep.briangonzalez.org/"
+			},
+			{
+				name: "Direction-Aware Hover Effect",
+				url: "http://tympanus.net/TipsTricks/DirectionAwareHoverEffect/index.html"
+			},
+			{
+				name: "OmniWindow - Extremely customizable modal window plugin",
+				url: "http://0x000000.github.com/OmniWindow/"
+			},
+			{
+				name: "jQuery Transit - Super-smooth CSS3 transformations and transitions for jQuery",
+				url: "http://ricostacruz.com/jquery.transit/"
+			},
+			{
+				name: "Photon - CSS 3D lighting engine",
+				url: "http://photon.attasi.com/"
+			},
+			{
+				name: "HorizontalNav - jQuery plugin that spans a horizontal navigation to fit the full width of it's container.",
+				url: "http://sebnitu.github.com/HorizontalNav/"
+			},
+			{
+				name: "Ascensor.js - jQuery plugin which aims to train and adapt content according to an elevator system",
+				url: "http://kirkas.ch/ascensor/"
+			}
+		]
+	},
+	{
+		category: "Libraries",
+		items: [
+			{
+				name: "Micro.js",
+				url: "http://microjs.com/"
+			},
+			{
+				name: "Sugar.js",
+				url: "http://sugarjs.com/"
+			},
+			{
+				name: "Modernizr",
+				url: "http://modernizr.com/"
+			},
+			{
+				name: "CAAT - Javascript animation toolkit",
+				url: "http://labs.hyperandroid.com/static/caat/"
+			},
+			{
+				name: "CCV - A Modern Computer Vision Library",
+				url: "http://libccv.org/post/introducing-ccv-milestone/"
+			},
+			{
+				name: "Annoying.js",
+				url: "http://kilianvalkhof.com/2011/javascript/annoying-js-how-to-be-an-asshole/"
+			},
+			{
+				name: "Underscore.js",
+				url: "http://documentcloud.github.com/underscore/"
+			},
+			{
+				name: "Fabric.js - makes it easy to work with HTML5 canvas element.",
+				url: "http://kangax.github.com/fabric.js/"
+			},
+			{
+				name: "Angular.js - HTML enhanced for web apps!",
+				url: "http://angularjs.org/"
+			},
+			{
+				name: "Meteor - A new way to build apps.",
+				url: "http://www.meteor.com/"
+			},
+			{
+				name: "jQuery++ is a collection of extremely useful DOM helpers and special events",
+				url: "http://jquerypp.com/"
+			},
+			{
+				name: "Toxiclibs.js - Open-source computational design library",
+				url: "http://haptic-data.com/toxiclibsjs/"
+			}
+		]
+	},
+	{
+		category: "Charts & Graphs",
+		items: [
+			{
+				name: "Sigma.js",
+				url: "http://sigmajs.org/"
+			},
+			{
+				name: "Morris.js",
+				url: "http://oesmith.github.com/morris.js/"
+			},
+			{
+				name: "Highcharts",
+				url: "http://www.highcharts.com/"
+			},
+			{
+				name: "Cubism.js - Time Series Visualization",
+				url: "http://square.github.com/cubism/"
+			},
+			{
+				name: "Peity - Progressive canvas pie charts",
+				url: "http://benpickles.github.com/peity/"
+			},
+			{
+				name: "Piecon - Pie charts in your favicon!",
+				url: "http://lipka.github.com/piecon/"
+			},
+			{
+				name: "NVD3 - Re-usable charts for d3.js",
+				url: "http://nvd3.com/"
+			},
+			{
+				name: "Simplify.js - Tiny high-performance JavaScript 2D/3D polyline simplification library.",
+				url: "http://mourner.github.com/simplify-js/"
+			},
+			{
+				name: "jsPlumb provides a way to plumb elements of a UI together.",
+				url: "http://jsplumb.org/jquery/demo.html"
+			}
+		]
+	},
+	{
+		category: "Forms",
+		items: [
+			{
+				name: "jQuery.suggest - Simple inline autosuggest jQuery plugin",
+				url: "http://polarblau.github.com/suggest/"
+			},
+			{
+				name: "Sisyphus - Keep your form data on refresh",
+				url: "http://simsalabim.github.com/sisyphus/"
+			},
+			{
+				name: "TextExt Plugin - Tag input and autocomplete.",
+				url: "http://textextjs.com/"
+			},
+			{
+				name: "jQuery Credit Card Validator",
+				url: "http://paweldecowski.github.com/jQuery-CreditCardValidator/"
+			}
+		]
+	},
+	{
+		category: "Other",
+		items: [
+			{
+				name: "limit.js - Limit JavaScript events & calls",
+				url: "http://limit.gotsomething.com/"
+			},
+			{
+				name: "jQuery PointPoint",
+				url: "http://tutorialzine.com/2011/09/jquery-pointpoint-plugin/"
+			},
+			{
+				name: "Tinycon - A small library for manipulating the favicon",
+				url: "http://tommoor.github.com/tinycon/"
+			},
+			{
+				name: "heatmap.js - Canvas based heatmaps",
+				url: "http://www.patrick-wied.at/static/heatmapjs/"
+			},
+			{
+				name: "Zoomooz.js - Make any web page zoom.",
+				url: "http://janne.aukia.com/zoomooz/"
+			},
+			{
+				name: "Hammer.js - Library for multi-touch gestures",
+				url: "http://eightmedia.github.com/hammer.js/"
+			},
+			{
+				name: "jPages - Pagination plugin",
+				url: "http://luis-almeida.github.com/jPages/"
+			},
+			{
+				name: "jQuery NailThumb - From any image to any thumbnail with full control",
+				url: "http://www.garralab.com/nailthumb.php"
+			},
+			{
+				name: "jQuery HiddenPosition - Easily position any element, even if hidden",
+				url: "http://www.garralab.com/hiddenposition.php"
+			},
+			{
+				name: "Handsontable - Excel-like table editor in HTML & jQuery.",
+				url: "http://warpech.github.com/jquery-handsontable/"
+			},
+			{
+				name: "jQuery.my - jQuery plugin that binds HTML with JS objects. In real-time.",
+				url: "http://jquerymy.com/"
+			},
+			{
+				name: "pageguide.js - An interactive guide for web page elements using jQuery and CSS3.",
+				url: "http://tracelytics.github.com/pageguide/"
+			},
+			{
+				name: "Holder.js renders image placeholders entirely on the client side.",
+				url: "http://imsky.github.com/holder/"
+			},
+			{
+				name: "trunk8.js - Text truncation extension to jQuery",
+				url: "http://jrvis.com/trunk8/"
 			}
 		]
 	}
 ];
 
-$("#pluginsTemplate").tmpl({
-	plugins: plugins
-}).appendTo('#plugins-sidebar');
-
+$('#plugins-categories').html($("#pluginsTemplate").tmpl(plugins));
 
 $('#plugins-sidebar').on('click', 'li', function(){
-	$('iframe').prop('src', $(this).data('url'));
+	if (!$(this).hasClass('selected')) {
+		var url = $(this).data('url');
+		$('iframe').prop('src', url);
+		$('#plugins-sidebar').find('li.selected').removeClass('selected');
+		$(this).addClass('selected');
+	}
 }).on('click', 'h2', function(){
+	$('#plugins-sidebar').find('h2.selected').removeClass('selected').end().find('ul').hide();
 	var isVisible = $(this).next().is(':visible');
-	$('#plugins-sidebar').find('ul').hide();
 	if (isVisible) {
 		$(this).next().hide();
 	} else {
 		$(this).next().show();
+		$(this).addClass('selected');
 	}
 });
+
+$('iframe').prop('src', "default-content.html");
