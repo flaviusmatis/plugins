61e444b8de3d40427ce7147128d81144d555ca09
Flavius Matis <flavius@fdbk.me>
2012-11-24 16:28:21 +0200

    Updating master.

---
+++ categories.js
@@ -0,0 +1,58 @@
+var categories = [
+	{
+		name: "Currency",
+		tag: "currency"
+	},
+	{
+		name: "Date",
+		tag: "date"
+	},
+	{
+		name: "Forms",
+		tag: "form"
+	},
+	{
+		name: "Highlighting",
+		tag: "highlight"
+	},
+	{
+		name: "HTML Editors",
+		tag: "editor"
+	},
+	{
+		name: "Libraries",
+		tag: "library"
+	},
+	{
+		name: "Maps",
+		tag: "map"
+	},
+	{
+		name: "Markdown",
+		tag: "markdown"
+	},
+	{
+		name: "Presentation, Sliders",
+		tag: "slide"
+	},
+	{
+		name: "Scrolling",
+		tag: "scroll"
+	},
+	{
+		name: "Templates",
+		tag: "template"
+	},
+	{
+		name: "UI, Effects",
+		tag: "ui"
+	},
+	{
+		name: "Visualization tools",
+		tag: "chart"
+	},
+	{
+		name: "Other...",
+		tag: "other"
+	}
+];
